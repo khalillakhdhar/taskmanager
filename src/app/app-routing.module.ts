447faa91bba87ab7ff4db2ebfa22de3cf09f681f
khalil <khalillakhdharatc@gmail.com>
2023-06-06 13:29:05 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'tasks',pathMatch:'full'},
+  {path:'tasks',component:TaskListComponent},
+  {path:'tasks/:index',component:TaskDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskDetailsComponent
   ],
   imports: [
     BrowserModule,
